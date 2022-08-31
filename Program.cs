d5a4e7c7c336b0ad35ea470d4fccc607a5cb3064
usernamenahaichuk <gely231@mail.com>
2022-08-31 15:19:01 +0300

    31/08

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿void MaxValue(int x, int y)
+{
+    Math.Max(x, y);
+}
+
+void MinValue(int x, int y)
+{
+    Math.Min(x, y);
+}
+
+bool TrySumIfOdd(int x, int y, out int Out)
+{
+    Out = x + y;
+    return ((x + y) % 2 == 0) ? false : true;
+}
+
+void Repeat(String x, int y)
+{
+    String t = ""; 
+    for (int i = 0; i < y; i++)
+    {
+        t += x;
+    }
+
+    Console.WriteLine(t);
+}
+
+Repeat("str", 3);
